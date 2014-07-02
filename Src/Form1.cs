a1197f362f5654b6687da9d52321bffbaea6eeda
Nakia <nakiaaresz@gmail.com>
2014-07-02 19:20:56 +0200

    adatok folyt.

--- Src/Form1.cs
+++ Src/Form1.cs
@@ -10,7 +10,7 @@ namespace erep
         public Form1()
         {
             InitializeComponent();
-            Skybound.Gecko.Xpcom.Initialize("xulrunner");
+            Skybound.Gecko.Xpcom.Initialize(@"..\..\Src\xulrunner");
             this.Text = "Auto fight";
             Alapok();
         }
@@ -39,7 +39,36 @@ namespace erep
             cim.Size = new Size(175, 22);
             cim.Location = new Point(300 - (175 / 2), 5);
             //-------------------------------------------------------
-
+            Label nev = new Label();
+            Controls.Add(nev);
+            nev.Text = "Felhasználó név:";
+            nev.Size = new Size(115, 22);
+            nev.Font = new System.Drawing.Font("Arial", 10);
+            nev.Location = new Point(100, 75);
+            //-------------------------------------------------------
+            TextBox nevt = new TextBox();
+            Controls.Add(nevt);
+            nevt.Location = new Point(220, 73);
+            nevt.Size = new Size(nevt.Size.Width + 50, 22);
+            nevt.Focus();
+            /*
+            TODO:LOAD
+            */
+            //-------------------------------------------------------
+            Label pass = new Label();
+            Controls.Add(pass);
+            pass.Text = "Jelszó:";
+            pass.Size = new Size(50, 22);
+            pass.Font = new Font("Arial", 10);
+            pass.Location = new Point(164, 100);
+            //-------------------------------------------------------
+            TextBox passt = new TextBox();
+            Controls.Add(passt);
+            passt.Location = new Point(220, 98);
+            passt.Size = nevt.Size;
+            /*
+            TODO:LOAD
+            */
         }
     }
 }
